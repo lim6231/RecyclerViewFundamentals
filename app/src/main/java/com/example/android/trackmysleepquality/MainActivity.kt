3b8effbe59dbfdfe929c26a1be894f9ee677d133
taruc <limew-pm17@student.tarc.edu.my>
2020-01-03 14:00:00 +0800

    init

--- app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
@@ -21,7 +21,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-//
+////
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
